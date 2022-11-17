c567a00a652a1d7531167021c9d53aa965118ca9
Kevin Tao <kevin.tao@stonybrook.edu>
2022-11-17 12:31:19 -0500

    tested a little bit

--- src/rugrep.rb
+++ src/rugrep.rb
@@ -7,19 +7,20 @@ def parseArgs(args)
   files = []; patterns = []
   startPats = false; endPats = false
   if args.length < 2 then puts 'USAGE: ruby rugrep.rb'; return end
-  puts args
+  puts args.join(", ")
   args.each do |arg|
     if arg.start_with?('\\') then # Regex
-      puts 'Regex Read'
+      #puts 'Regex Read'
       if not startPats then startPats = true end
       if endPats then puts 'USAGE: ruby rugrep.rb'; return end
       reg = arg[1..arg.length-2]
       begin
         regex = Regexp.new(reg)
         patterns.append(reg)
-      rescue puts('Error: cannot parse regex') end
+      rescue => e 
+        puts('Error: cannot parse regex'); end
     elsif arg.start_with?('-') then #Option
-      puts 'Option Read'
+      #puts 'Option Read'
       if startPats then endPats = true end
       if arg == '-v' or arg == '--invert-match' then optv = true
       elsif arg == '-c' or arg == '--count' then optc = true
@@ -41,12 +42,12 @@ def parseArgs(args)
       elsif arg.start_with?('--context=') then
         optA = arg.sub('--context=','').to_i
         optB = arg.sub('--context=','').to_i
-      else then 
+      else 
         puts 'USAGE: ruby rugrep.rb' 
         return
       end
     else #Filepath or error
-      puts 'Other Read'
+      #puts 'Other Read'
       if startPats then endPats = true end
       unless not (File.directory?(arg)) then files.append(arg) end
     end
@@ -61,78 +62,93 @@ def parseArgs(args)
         matched = false
         lposmatch = []; lnegmatch = []; lcount = 0
         iter = 0
-        IO.foreach("#{path}#{fn}", "r") do |line|
+        #puts "fn: #{fn}"
+        IO.foreach("#{path}#{fn}") do |line|
+          #puts "line: #{line} endline"
           if optF then #Fixed string match
             patterns.each do |pat|
-              if line.include?(pat) then 
+              if line.include?("#{pat}") then 
+                #puts "here"
                 if opto then 
                   lposmatch.concat(line.scan(pat))
-                else then 
+                else 
+                  #puts "here2"
                   lposmatch.append(line) 
+                  #puts lposmatch.join(" ")
+                  #puts "endhere2"
                 end
-              else then 
+              else 
+                #puts "here3"
                 lnegmatch.append(line) 
+                #puts lnegmatch.join(" ")
+                #puts "endhere3"
               end 
             end 
           elsif optA != 0 or optB != 0 then
             patterns.each do |pat|
               patreg = Regexp.new(pat)
               if line =~ patreg then lposmatch.append(iter)
-              else then lnegmatch.append(iter) end
+              else lnegmatch.append(iter) end
             end
-          else then
+          else
             patterns.each do |pat|
               patreg = Regexp.new(pat)
-              if line.include?(patreg) then 
+              if line =~ patreg then 
                 lcount += 1
                 lposmatch.concat(line.scan(patreg))
+              end
             end
           end 
           iter += 1
         end
+        lposmatch = lposmatch.uniq
+        lnegmatch = lnegmatch.uniq
         if lposmatch != 0 then fposmatch.append("#{path}#{fn}")
-        else fnegmatch.append("#{path}#{fn}")
+        else fnegmatch.append("#{path}#{fn}") end
         if optF then #-F
           if optc then #-c -v or -c
+            #puts "optFc or optFcv"
             if optv then puts lnegmatch.length
-            else then puts lposmatch.length end
+            else puts lposmatch.length end
           elsif optv then #-v
+            #puts "optFv"
             puts lnegmatach
           elsif opto then #-o
+            #puts "optFo"
             puts lposmatch
-          else then #no args
+          else #no args
+            #puts "optF"
             puts lposmatch
           end  
-        
         elsif optA != 0 or optB != 0 then
           a = IO.readlines("#{path}#{fn}")
           if optv then
             fnegmatch.each do |index|
               puts a[index-optB...index+optA]
-              if fnegmatch.length > 1 then puts '--'
+              if fnegmatch.length > 1 then puts '--' end
             end
-          else then
+          else
             fposmatch.each do |index|
               pre = index-optB
               if pre < 0 then pre = 0 end
               puts a[pre...index+optA]
-              if fposmatch.length > 1 then puts '--'
+              if fposmatch.length > 1 then puts '--' end
             end
           end
         elsif opto then
           if optc then puts lcount
-          else then puts lposmatch end
+          else puts lposmatch end
         elsif optc then
           if optv then puts "#{path}#{fn}: #{iter-lcount}"
-          else then puts "#{path}#{fn}: #{lcount}" end
+          else puts "#{path}#{fn}: #{lcount}" end
+        elsif not optl and not optL 
+          puts lposmatch
         end 
-
-
-
       end
-    rescue puts "Error: could not read file #{path}" end
+    rescue => e 
+      puts("Error: could not read file #{path}"); end
   end
-  if opl then puts fposmatch # -l
+  if optl then puts fposmatch # -l
   elsif optL then puts fnegmatch end # -L
 end
 
